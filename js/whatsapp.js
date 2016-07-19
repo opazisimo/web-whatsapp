864591c9fff38d7ae662415cb05529a8bf8f473e
SigridMonsalve <n7.monsalve@gmail.com>
2016-07-19 09:08:20 -0400

    Agregado emoticons

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,22 +1,45 @@
 $(document).ready(function() {
-	$('.icon-mic').click(function(e){
-		var inputChat = $('#inputChat');
-		var minIcon=$('.minIcon');
-		minIcon.click(function(){
-			input.Chat.append('minIcon');
-		});
-		if(inputChat.val.length > 0 ){
-			var chat = $('.chat');
-			var chatingresado = $('#inputChat').val();
-			chat.append('<div class="row"><div class="col-lg-offset-6 col-lg-6"><span class="chatboxUser"><p class="detalle">'+ chatingresado +'</p></span></div></div>')
-			inputChat.val('');
-		}
-});
-
-var usuarios=[]
-var clicked;
-usuarios=['Laboratoria Perú', 'Raymi Saldomando','Mariana Costa','Ana María Martínez','Rodulfo Prieto','Andrea Lamas','Maria Paula Rivarola','Katy Sanchez','Aldo Alfaro'];
+	var usuarios=[]
+	var clicked;//Variable que reconoce si es el primer o segundo click que se hace.
+	var addIcon;
+	var writing;
+	usuarios=['Laboratoria Perú', 'Raymi Saldomando','Mariana Costa','Ana María Martínez','Rodulfo Prieto','Andrea Lamas','Maria Paula Rivarola','Katy Sanchez','Aldo Alfaro'];
 	clicked=false;
+	addIcon=false;
+	writing=false;
+	var inputChat = $('#inputChat');
+	var minIcon=$('.minIcon');
+	minIcon.click(function(){
+		minIcon=$(this);
+		minIcon.clone().appendTo(inputChat);
+		addIcon=true;
+	});
+	inputChat.click(function(){
+		while (writing==false){
+			inputChat.html('');
+			inputChat.css('color','black');
+			writing=true;
+		}
+		
+	});
+	$('.icon-mic').click(function(e){
+		if(addicon=true){
+			chatingresado=inputChat.html();
+		}
+		else{
+			chatingresado=inputChat.text();
+		}
+		
+		if(inputChat.length > 0 ){
+			var chat = $('.chat');
+			chat.append('<div class="row"><div class="col-lg-offset-6 col-lg-6"><span class="chatboxUser"><p class="detalle">'+ chatingresado +'</p></span></div></div>')
+			inputChat.css('color','#AAAAAA');
+			inputChat.html('Escribe un mensaje...');
+			addIcon=false;
+			writing=false;
+
+		}
+	});
 	$('.contactos>div').click(function(){
 		$('.chat').empty();
 	});
